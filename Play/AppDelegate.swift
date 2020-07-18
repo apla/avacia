39d5f0398519e4f7ade55d2d8d0e4deea49c1fb2
ivan baktsheev <dot.and.thing@gmail.com>
2020-07-18 10:49:15 +0300

    Initial Commit

---
+++ Play/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Play
+//
+//  Created by apla on 18/07/2020.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
